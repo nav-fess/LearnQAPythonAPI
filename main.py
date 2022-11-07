8f0726fea1a404e48e7f473fe40fae42f2b1b556
Vladimir Voronin <nav-fess@yandex.ru>
2022-11-07 13:35:41 +0300

    refactoring function print_text_api

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
+def print_text_api(link):
+    response = requests.get(link)
+    print(response.text)
 
-import requests
 
-response = requests.get("https://playground.learnqa.ru/api/hello")
-print(response.text)
+link_hello = "https://playground.learnqa.ru/api/hello"
+print_text_api(link_hello)
+link_get_text = "https://playground.learnqa.ru/api/get_text"
+print_text_api(link_get_text)
